253f89b5291a2d8370b049c58ae3e112ee0151ec
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-05-29 11:02:02 +0630

    completed: main section

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -4,19 +4,19 @@ import Button from "../common/Buttons";
 
 export default function MainSection() {
   return (
-    <section>
-      <div className="main">
-        <h1>
+    <section className="grid grid-cols-1 gap-3 items-center justify-items-center px-4 tablet:grid-cols-2 laptop:grid-cols-2 desktop:grid-cols-2 ">
+      <div className="main w-full desktop:pl-[5.3rem] laptop:pl-[5rem] tablet:pl-[4.5rem]">
+        <h1 className="text-6xl leading-normal font-bold">
           Transforming
           <br /> Digitally,
           <br /> Together
         </h1>
-        <div>
-          <Button type="primary" text="Explore" className="w-[7rem]" />
-          <Button type="secondary" text="Showcase" className="w-[7rem]" />
+        <div className="flex flex-row gap-4 items-center m-2">
+          <Button type="primary" text="Explore" className="w-32" />
+          <Button type="secondary" text="Showcase" className="w-32" />
         </div>
       </div>
-      <Image src={MainImage} alt="Main Image" />
+      <Image src={MainImage} alt="Main Image" height={850} width={850} />
     </section>
   );
 }
