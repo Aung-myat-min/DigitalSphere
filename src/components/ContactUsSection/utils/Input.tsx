4a7f3e4726e33d5ec4d95aba0a9eca152e7624ef
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-05-30 16:46:23 +0630

    completed: contact us

---
+++ src/components/ContactUsSection/utils/Input.tsx
@@ -0,0 +1,30 @@
+"use client";
+interface InputProps {
+  labelText: string;
+  required: boolean;
+  type: "text" | "email" | "tel";
+  name: string;
+  id: string;
+}
+export default function Input({
+  labelText,
+  required,
+  type,
+  name,
+  id,
+}: InputProps) {
+  return (
+    <div className="flex flex-col gap-3">
+      <label htmlFor={id} className="text-[16px]">
+        {labelText} {required ? "*" : "(Optional)"}
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        required={required}
+        className="bg-inputBg focus:bg-[#fff] focus:border-primary font-medium text-[22px] rounded-2xl py-3 px-4 h-[75px]"
+      />
+    </div>
+  );
+}
---
+++ src/components/ContactUsSection/utils/TextArea.tsx
@@ -0,0 +1,27 @@
+"use client";
+interface TextAreaProps {
+  labelText: string;
+  required: boolean;
+  name: string;
+  id: string;
+}
+export default function TextArea({
+  labelText,
+  required,
+  name,
+  id,
+}: TextAreaProps) {
+  return (
+    <div className="flex flex-col gap-3">
+      <label htmlFor={id} className="text-[16px]">
+        {labelText} {required ? "*" : "(Optional)"}
+      </label>
+      <textarea
+        name={name}
+        id={id}
+        required={required}
+        className="bg-inputBg focus:bg-[#fff] focus:border-primary font-medium text-[22px] rounded-2xl py-3 px-4 h-[75px]"
+      />
+    </div>
+  );
+}
